5cbbdc494f650d200aedb36757f912485fd0da01
Shujath <shujath786@gmail.com>
2025-02-21 16:23:01 -0500

    updated words table ```

--- lang-portal/backend-flask/sql/setup/create_table_words.sql
+++ lang-portal/backend-flask/sql/setup/create_table_words.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS words (
   english TEXT NOT NULL,
   arabic TEXT NOT NULL,
   root TEXT NOT NULL,
+  transliteration TEXT NOT NULL,
   parts TEXT NOT NULL  -- Store parts as JSON string
 );
